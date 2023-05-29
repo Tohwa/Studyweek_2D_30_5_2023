3b3bcd8a93862c18a8c16067e0b7dc7faf71ca1b
Sascha <Tohwa.gaming@gmail.com>
2023-05-29 22:03:44 +0200

    added distribution of grass/dirt/stone layers via generator

--- Studyweek_2D/Assets/TerrainGeneration.cs
+++ Studyweek_2D/Assets/TerrainGeneration.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class TerrainGeneration : MonoBehaviour
 {
     #region Fields
-    [SerializeField] private Sprite tile;
+    [SerializeField] private Sprite stone;
+    [SerializeField] private Sprite dirt;
+    [SerializeField] private Sprite grass;
+    [SerializeField] private Sprite log;
+    [SerializeField] private Sprite leaf;
+
+    [SerializeField] private bool generateCaves = true;
+    [SerializeField] private int dirtLayerHeigt = 5;
     [SerializeField] private float surfaceValue = 0.25f;
     [SerializeField] private float heightMultiplier = 4f;
     [SerializeField] private int heightAddition = 25;
@@ -31,16 +38,33 @@ public class TerrainGeneration : MonoBehaviour
 
             for (int y = 0; y < height; y++)
             {
-                if (noiseTexture.GetPixel(x, y).r > surfaceValue)
+                Sprite tileSprite;
+                if(y < height - dirtLayerHeigt)
                 {
-                    GameObject newTile = new GameObject(name = "tile");
-                    newTile.transform.parent = this.transform;
-                    newTile.AddComponent<SpriteRenderer>();
-                    newTile.GetComponent<SpriteRenderer>().sprite = tile;
-                    newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
+                    tileSprite = stone;                    
+                }
+                else if(y < height - 1)
+                {
+                    tileSprite = dirt;
+                }
+                else
+                {
+                    tileSprite = grass;
+                }
+
+                if (generateCaves)
+                {
+                    if (noiseTexture.GetPixel(x, y).r > surfaceValue)
+                    {
+                        PlaceTile(tileSprite, x, y);
+                    }
+                }
+                else
+                {
+                    PlaceTile(tileSprite, x, y);
                 }
                 
-            }
+            }            
         }
     }
 
@@ -58,5 +82,15 @@ public class TerrainGeneration : MonoBehaviour
         }
 
         noiseTexture.Apply();
+    }
+
+    public void PlaceTile(Sprite tileSprite, float x, float y)
+    {
+        GameObject newTile = new GameObject();
+        newTile.transform.parent = this.transform;
+        newTile.AddComponent<SpriteRenderer>();
+        newTile.GetComponent<SpriteRenderer>().sprite = tileSprite;
+        newTile.name = tileSprite.name;
+        newTile.transform.position = new Vector2(x + 0.5f, y + 0.5f);
     }
 }
